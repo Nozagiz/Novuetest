9503036367a0ce5a6979fbe7472f92d0a1b41c33
Nozagiz <83748136+Nozagiz@users.noreply.github.com>
2021-09-25 15:41:52 +0700

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template >
+<div>
+  <img alt="Vue logo" src="./assets/logo2.png">
+  <h3><HelloWorld msg="Welcome to My world"/></h3>
+  <h5><HelloWorld msg="Nutchanon  Thawornnuruk" major="Information Technology and Digital Innovation"/></h5>
+  <br/>
+  <button @click="counter += 1"> Add 1</button> 
+  <p>The button above has been clicked {{ counter }} times.</p>
+  <input v-model="message" placeholder="enter">
+  <p>Message is: {{ message }}</p>
+ </div> 
+  
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      counter:0,
+      message:""
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="hello" >
+    <h1>{{ msg }}</h1>
+    <h2>{{ major }}</h2>
+   <!-- <button @click="counter += 1"> Add 1</button> 
+  <p>The button above has been clicked {{ counter }} times.</p> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+    major: String,
+  },
+  // data(){
+  //   return {
+  //     counter:0
+  //   }
+  // }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
